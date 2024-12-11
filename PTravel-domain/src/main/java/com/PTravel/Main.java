c70a286b678d4b3dc7bc15a0d63e0b78a06dc27d
PhucNH <phucnh0303@gmail.com>
2024-12-11 23:22:32 +0700

    - dev: create base structure for project use DDD architecture

---
+++ PTravel-domain/src/main/java/com/PTravel/Main.java
@@ -0,0 +1,7 @@
+package com.PTravel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- PTravel-start/src/main/java/com/PTravel/StartApplication.java
+++ PTravel-start/src/main/java/com/PTravel/StartApplication.java
@@ -2,10 +2,18 @@ package com.PTravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/hello")
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
+
+    @GetMapping("/hi")
+    String hello() {return "Hello";}
 }
